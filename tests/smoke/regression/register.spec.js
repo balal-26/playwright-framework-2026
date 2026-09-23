e70f227e120eac3fd0c5723704065e74f1e53cab
balal-26 <ahmadtosca786@gmail.com>
2026-09-23 22:51:13 +0530

    Update register.spec.js

--- tests/smoke/regression/register.spec.js
+++ tests/smoke/regression/register.spec.js
@@ -4,4 +4,9 @@ test.describe('Register', () => {
   test('should register a new user', async ({ page }) => {
     await page.goto('https://example.com/register');        
   });
-});
+
+  test('should register a new user from git hub', async ({ page }) => {
+    await page.goto('https://example.com/registernew');        
+  });
+
+  
